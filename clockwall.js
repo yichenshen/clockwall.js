9669a8f06aa9fab892bad7a6eaeb5b29796d1152
Shen Yichen <2007.yichen@gmail.com>
2015-06-11 22:28:17 +0800

    Removed callbacks
    Switched from using animation callbacks to delay based queue. This means that all of the data is set up the start, which reduces computational load during animation.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- clockwall.js
+++ clockwall.js
@@ -15,16 +15,14 @@
     var NUM_CLOCKS_Y = 20;
     var CLOCK_SIZE = 15;
     var HR_TO_MIN_RATIO = 0.9;
+    var START_DELAY = 1500;
 
     function Clock(x, y, paper) {
         this.x = x;
         this.y = y;
 
-        this.minAngle = 0;
-        this.hrAngle = 0;
-
-        this.minCount = 0;
-        this.hrCount = 0;
+        this.minDelay = 0;
+        this.hrDelay = 0;
 
         this.paper = paper;
 
@@ -34,50 +32,54 @@
         this.hrHand = paper.path("M" + x + "," + y + "l0," + CLOCK_SIZE * HR_TO_MIN_RATIO).attr({"stroke-width": "2"});
     }
 
-    Clock.prototype.turn = function(hand, angle, time, endFunc) {
-        if(angle === 0){
-            TweenLite.delayedCall(time/1000, endFunc(this));
-        } else{
-            console.log(time/1000);
-            TweenLite.to(this[hand+"Hand"], time/1000, {raphael:{rotation: "+="+angle, globalPivot:{x: this.x, y: this.y}}, ease:Linear.easeNone, onComplete: endFunc(this)});
-        }
+    Clock.prototype.queueTurnMin = function(angle, time) {
+        
+        TweenLite.to(this.minHand, time/1000, {raphael:{rotation: "+="+angle, globalPivot:{x: this.x, y: this.y}}, ease:Linear.easeNone, delay:this.minDelay/1000});
+
+        this.minDelay += time;
     };
 
-    Clock.prototype.resetCount = function(){
-        this.minCount = 0;
-        this.hrCount = 0;
-    };
+    Clock.prototype.queueTurnHr = function(angle, time){
+        TweenLite.to(this.hrHand, time/1000, {raphael:{rotation: "+="+angle, globalPivot:{x: this.x, y: this.y}}, ease:Linear.easeNone, delay:this.hrDelay/1000});
 
-    Clock.prototype.resetAngle = function(){
-        this.minAngle = 0;
-        this.hrAngle = 0;
-    };
+        this.hrDelay += time;
+    }
 
     function Scheme(minData, hrData){
         this.minData = minData;
         this.hrData = hrData;
     }
 
-    Scheme.prototype.callBackFunction = function(x, y, hand){
-        
-        var list = this[hand + "Data"][x][y];
+    Scheme.prototype.animate = function(clocks){
+        for(var i = 0; i < clocks.length; i++){
+            for(var j = 0; j < clocks[i].length; j++){
+                var minList = this.minData[i][j];
+               
+                for(var k = 0; k < minList.length; k++){
+                   clocks[i][j].queueTurnMin(minList[k][0], minList[k][1]);
+                }
 
-        var func = function(clock){
-            return function(){
-                var count = clock[hand+"Count"];
-                if(count < list.length){
-                    var instruct = list[count];
-                    clock[hand+"Count"]++;
-                    clock.turn(hand, instruct[0],instruct[1], func);
+                var hrList = this.hrData[i][j];
+
+                for(var k = 0; k < hrList.length; k++){
+                    clocks[i][j].queueTurnHr(hrList[k][0], hrList[k][1]);
                 }
             }
-        };
-
-        return func;
+        }
     };
 
     env.Clockwall = function(container, width, height) {
+
         this.paper = Raphael(container, width, height);
+
+        var clocks = new Array(NUM_CLOCKS_X);
+
+        for(var i = 0; i < NUM_CLOCKS_X; i++){
+            clocks[i] = new Array(NUM_CLOCKS_Y);
+            for(var j = 0; j < NUM_CLOCKS_Y; j++){
+                clocks[i][j] = new Clock(CLOCK_SIZE + i * 2 * CLOCK_SIZE, CLOCK_SIZE + j * 2 * CLOCK_SIZE, this.paper);
+            };
+        };
 
         var select = Math.floor(Math.random()*schemes.length);
 
@@ -85,14 +87,8 @@
         var minScheme = schemes[select].minHand;
 
         var s = new Scheme(minScheme,hrScheme);
-        
-        for(var i = 0; i < NUM_CLOCKS_X; i++){
-            for(var j = 0; j < NUM_CLOCKS_Y; j++){
-                var clock = new Clock(CLOCK_SIZE + i * 2 * CLOCK_SIZE, CLOCK_SIZE + j * 2 * CLOCK_SIZE, this.paper);
-                s.callBackFunction(i, j, "min")(clock)();
-                s.callBackFunction(i, j, "hr")(clock)();
-            };
-        };
+        TweenLite.delayedCall(START_DELAY/1000, s.animate, [clocks], s);       
     }
 
 })(this);
+
